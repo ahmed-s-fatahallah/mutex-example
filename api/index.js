1f42c9bad8246cf99edc85b08b2ad5fbeaa266fc
Ahmed Saeed <115148623+ahmed-s-fatahallah@users.noreply.github.com>
2024-08-26 17:50:43 +0300

    Add files via upload

--- api/index.js
+++ api/index.js
@@ -1,22 +1,31 @@
-import * as http from "node:http";
+import cors from "cors";
+import express from "express";
+
+const corsOptions = {
+  origin: "*", // Replace with your client's origin
+  methods: "GET,POST,PUT,DELETE,OPTIONS",
+  allowedHeaders: "Content-Type,Authorization",
+};
 
 let counter = 0;
 
-const server = http.createServer((req, res) => {
-  res.writeHead(
-    200,
-    { "Content-Type": "application/json" },
-    { "access-control-allow-origin": "*" }
-  );
+const app = express();
+app.use(express.json());
+app.use(cors(corsOptions));
+
+app.get("/", async (_, res) => {
   setTimeout(() => {
-    res.end(
-      JSON.stringify({
-        count: ++counter,
-      })
-    );
-  }, 3000);
+    res.status(200).json({ counter });
+  }, 1000);
 });
 
-server.listen(3000).on("listening", () => {
-  console.log("server is running on port 3000");
+app.post("/", async (req, res) => {
+  counter = req.body.counter;
+  setTimeout(() => {
+    res.status(200).json({ counter });
+  }, 1000);
+});
+
+app.listen(3000, () => {
+  console.log("server is running on port: 3000");
 });
